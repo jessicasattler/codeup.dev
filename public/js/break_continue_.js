fefbbe4108d12eaa1c6bfb96d79b2d24645e6d44
Jessica Sattler <jessicasattler28@gmail.com>
2016-07-29 16:14:12 -0500

    Break _ continue exercises

---
+++ public/js/break_continue_.js
@@ -0,0 +1,72 @@
+'use strict'
+//This will evaluate what house we are in
+
+// var house = "Griffendor"
+
+// switch(house){
+// 	case "Griffendor":
+// 	console.log("Known for the integrity, strength, and courage.");
+// 	break;
+
+// 	case "Slytherin":
+// 	console.log("Known for their ambition");
+// 	break;
+
+// 	case "Hufflepuff":
+// 	console.log("Being red shirts");
+// 	break;
+
+// 	case "Ravenclaw":
+// 	console.log("Smart butts");
+// 	break;
+// }
+
+// var random = Math.floor((Math.random()*100)+1);
+
+// console.log('Random stopping point is: ' + random);
+
+// for (var i = 1; i < 100; i++) {
+
+//     console.log('Loop counter is: ' + i);
+
+//     if (random == i) {
+
+//         console.log('We have reached the random stopping point: break!');
+
+//         // use the break keyword to exit from the while loop
+//         break;
+
+//         // nothing after the break will get processed
+//         console.log('You will never see this line.');
+//     }
+// }
+
+var randomNumber = Math.floor((Math.random()*50)+1);
+//creates a random number between 1 and 5
+
+ 	console.log("Random odd number to skip is: " + randomNumber)
+ 
+
+for (var i = 1; i < 50; i++) {
+
+    if (i % 2 == 0) {
+        
+        // this is saying that if i is even, then
+        //skip the rest of the loop and continue with the next iteration (meaning go back to the top and
+        //run again until you get i be an odd number)
+
+        continue;
+    }
+
+    if (i == randomNumber){
+    	console.log('Yikes! Skipping number ' + randomNumber);
+    	continue;
+
+    }
+
+    console.log('Here is a lovely odd number: ' + i);
+
+        
+    
+ }
+
--- public/js/for_loop.js
+++ public/js/for_loop.js
@@ -38,22 +38,38 @@
 // var n = num.toString();
 
 for (var i = 1; i <=10 ; i += 1) {
+	//("i" means the loop will start at 1; the loop will continue as long as "i" is 
+	//less than or equal to 10; "i" will increase by 1 after each iteration of the loop.  
 
 	var num = i.toString();
+	//we create a variable called num and this variable turns "i" into a string. 
 	num = num.repeat(i);
+	//when we turn this variable into a string, we can have it repeat "i" number of times
+	
 	
 	if (i == 10){
 		num = "0";
 		num = num.repeat(i);
 
-
+//we want to add this if statement because we want to end up with ten zeroes and to do this, 
+//we would have an if statement that tells us , if i is equal to "10" (but not assigning a 
+//a value of 10 to it), then set the variable "num" to now be equal to zero. Once we have
+// this, we say num is equal to num.repeat (i) so that we repeat the number zero ten times. 
 
 	}
 	console.log(num);
-}
 
+//we want to use console log at the end so that it can go through the loop and then give 
+//us the desired output, including the 
+}
+//-------------------------------------------------------------------------
 
 // for # 3
+//Write the JS code to log to the console the multiplication table for 
+//a random number between 1 and 10. 
+//For instance, if the random number is 7, the console output should look like
+
+
 var randomNumber = Math.floor(Math.random() * 10) + 1;
 
 for (var i = 1 ; i <= 10; i += 1 ) {
@@ -62,7 +78,8 @@ console.log(randomNumber + " x " + i + " =");
 console.log (randomNumber * i);
 
 }
-
+//-------------------------------------------------------------------
+//Exercise 4
 // Using a for loop and the code to generate a random number from the 
 // previous lessons, generate 10 random numbers between 20 and 200 and output to the 
 // console whether each number is odd or even. For example:
@@ -71,6 +88,9 @@ console.log (randomNumber * i);
 // 24 is even
 // 199 is odd
 // ...
+
+//-------------------------
+//
 
 
 
@@ -86,6 +106,10 @@ var randomNumber = Math.floor(Math.random() * 200) + 20;
 }
 
 
+//Exercise 5
+//Use inline JavaScript to create a
+// for loop that uses console.log to 
+//create the output shown below.
 for (var i = 100; i >= 5 ; i -= 5){
 
 	console.log(i);
