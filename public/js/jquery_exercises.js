1dc355fc0d5ba5c47f2ff329b84789cbcd13e650
Jessica Sattler <jessicasattler28@gmail.com>
2016-08-11 14:36:56 -0500

    jQuery mouse events exercise

--- public/js/jquery_exercises.js
+++ public/js/jquery_exercises.js
@@ -6,15 +6,63 @@
 
 //or even better
 $(document).ready(function() {
-     var contents = $('#codeup');
-     alert(contents.html());
+
+	//code for Id Exercise 
+     // var contents = $('#codeup');
+     // alert(contents.html());
 
 
-     var obtainID = $('#paragraph');
-     alert(obtainID.html());
+     // var obtainID = $('#paragraph');
+     // alert(obtainID.html());
 
-     var obtainID2 = $('#finalMessage');
-     alert(obtainID2.html());
+     // var obtainID2 = $('#finalMessage');
+     // alert(obtainID2.html());
 
-  
+     //code for Class Exercise
+     // $('.codeup').css('border', '1px solid red');
+
+     //code for Element Selector
+    //  $('li').css('font-size', '20px');
+  		
+  	 // $('h1, p , li').css('border', '1px solid yellow');
+
+  	 // var obtainH1 = $('h1');
+  	 // alert(obtainH1.html());
+
+  	 //code for multiple selector :
+  	
+      // $('.codeup, p').css('background-color', '#FF0');
+
+      // $('*').css('border', '1px solid #F00');
+
+      //code for Mouse Events:
+	// Add jQuery code that will change the 
+	// background color of a h1 element when clicked
+
+	$('h1').click(function(){
+		$(this).css('background-color', 'blue');
+	});
+
+	//Make all paragraphs 
+	//have a font size of 18px when they are double clicked
+
+	$('p').dblclick(function(){
+		$(this).css('font-size','18px');
+
+	});
+
+	//Set all li text color to red when the mouse 
+	//is hovering, reset to white when it is not
+
+	$('li').hover(
+		function(){
+			$(this).css('color', 'red');
+		},
+		function(){
+			$(this).css('color','white');
+		}
+
+	);
 });
+
+
