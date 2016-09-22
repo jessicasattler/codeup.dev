a9ccc662677cdf891387ac48e467c56b1cb315c2
Jessica Sattler <jessicasattler28@gmail.com>
2016-09-22 15:07:50 -0500

    pdo_test.php works

--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,8 @@
 // $dbc = new PDO('mysql:host=127.0.0.1;dbname=database_name', 'USERNAME', 'PASSWORD');
 
 // Get new instance of PDO object , configures to employyes database:
-$dbc = new PDO('mysql:host=127.0.0.1;employees', 'vagrant', 'vagrant');
+
+$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,DB_USER,DB_PASS);
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -13,3 +14,4 @@ $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
 
 //we should get 127.0.0.1 via TCP/IP if it connected succesfully
+
--- pdo_test.php
+++ pdo_test.php
@@ -1,5 +1,11 @@
 <?php  
 // <!-- Use require to include db_connect.php. -->
+
+define ("DB_HOST", "127.0.0.1");
+define ("DB_NAME", "employees");
+define ("DB_USER", "vagrant");
+define ("DB_PASS", "vagrant");
+
 require ('db_connect.php');
 
 // <!-- Echo the PDO connection status -->
